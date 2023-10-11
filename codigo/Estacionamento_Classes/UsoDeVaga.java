1ea07c9305e554e20ddee52f68461f5f4d6a5e3c
Gabrielle Lira <79920434+GabiLira@users.noreply.github.com>
2023-10-11 12:12:08 -0300

    Update UsoDeVaga.java
    Codigo criado na primeira semana do projeto no meu branch (Gabi). As alterações que fiz foram colocadas no Main agora, mas criadas na data correta no meu branch.

--- codigo/Estacionamento_Classes/UsoDeVaga.java
+++ codigo/Estacionamento_Classes/UsoDeVaga.java
@@ -1,25 +1,43 @@
-
+import java.time.LocalDateTime;
+import java.time.Duration;
 
 public class UsoDeVaga {
 
-	private static final double FRACAO_USO = 0.25;
-	private static final double VALOR_FRACAO = 4.0;
-	private static final double VALOR_MAXIMO = 50.0;
-	private Vaga vaga;
-	private LocalDateTime entrada;
-	private LocalDateTime saida;
-	private double valorPago;
+    private static final double FRACAO_USO = 0.25;
+    private static final double VALOR_FRACAO = 4.0;
+    private static final double VALOR_MAXIMO = 50.0;
 
-	public UsoDeVaga(Vaga vaga) {
-		
-	}
+    private Vaga vaga;
+    private LocalDateTime entrada;
+    private LocalDateTime saida;
+    private double valorPago;
 
-	public double sair() {
-		
-	}
+    public UsoDeVaga(Vaga vaga) {
+        this.vaga = vaga;
+        this.entrada = LocalDateTime.now();
+        this.saida = null;
+        this.valorPago = 0.0;
+    }
 
-	public double valorPago() {
-		
-	}
+    public void sair() {
+        this.saida = LocalDateTime.now();
+        long minutos = Duration.between(entrada, saida).toMinutes();
+        
+        if (minutos <= 15) {
+            this.valorPago = 0.0;
+        } else {
+            int horas = (int)Math.ceil((double)minutos / 60);
+            if (horas <= 1) {
+                this.valorPago = VALOR_FRACAO;
+            } else if (horas >= 9) {
+                this.valorPago = VALOR_MAXIMO;
+            } else {
+                this.valorPago = VALOR_FRACAO + (horas - 1) * VALOR_FRACAO;
+            }
+        }
+    }
 
+    public double getValorPago() {
+        return valorPago;
+    }
 }
