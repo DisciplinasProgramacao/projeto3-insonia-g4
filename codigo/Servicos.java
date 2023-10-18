7d6117a6c65a6fada8cb489d80781515cd7cdc01
Gabrielle Lira <79920434+GabiLira@users.noreply.github.com>
2023-10-18 19:22:27 -0300

    Create Servicos.java
    Classe Servicos, que será chamada pela classe UsoDeVaga. Desenvolvida por mim e pelo LuizVictorAldenucci em conjunto.

---
+++ codigo/Servicos.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Servicos {
+    private int idServico;
+    private String nomeDoServico;
+    private double custoServico;
+    private int tempoMinimo;
+
+    public Servicos(int idServico, String nomeDoServico, double custoServico, int tempoMinimo) {
+        this.idServico = idServico;
+        this.nomeDoServico = nomeDoServico;
+        this.custoServico = custoServico;
+        this.tempoMinimo = tempoMinimo;
+    }
+
+    public static Servicos selecionarServico() {
+        System.out.println("Escolha o serviço (0 a 3): ");
+        System.out.println("0 - Nenhum");
+        System.out.println("1 - Manobrista");
+        System.out.println("2 - Lavagem");
+        System.out.println("3 - Polimento");
+
+        Scanner scanner = new Scanner(System.in);
+        int escolha = scanner.nextInt();
+
+        switch (escolha) {
+            case 0:
+                return new Servicos(0, "Nenhum", 00.0, );
+            case 1:
+                return new Servicos(1, "Manobrista", 5.0, 0);
+            case 2:
+                return new Servicos(2, "Lavagem", 20.0, 60);
+            case 3:
+                return new Servicos(3, "Polimento", 45.0, 120);
+            default:
+                throw new IllegalArgumentException("Escolha inválida. Por favor, escolha entre 0 e 3.");
+        }
+    }
+
+    public int getIdServico() {
+        return idServico;
+    }
+
+    public String getNomeDoServico() {
+        return nomeDoServico;
+    }
+
+    public double getCustoServico() {
+        return custoServico;
+    }
+
+    public int getTempoMinimo() {
+        return tempoMinimo;
+    }
+}
