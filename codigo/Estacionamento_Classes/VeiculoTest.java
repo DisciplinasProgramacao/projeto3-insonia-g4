ee692d74f426b774fdd3abeb7494caac482991fa
gabriel6988 <83712515+gabriel6988@users.noreply.github.com>
2023-10-23 11:10:26 -0300

    Add files via upload

---
+++ codigo/Estacionamento_Classes/VeiculoTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Before;
+import org.junit.jupiter.api.Test;
+
+public class VeiculoTest{
+    private Veiculo veiculo;
+    private Vaga vaga;
+
+    @Before
+    public void setUp(){
+        vaga = new Vaga(1, 1);
+        veiculo = new Veiculo("1234567");
+        veiculo.estacionar(vaga);
+    }
+
+    @Test
+    public void testSair(){
+        assertEquals(0.0, veiculo.sair());
+    }
+
+    @Test
+    public void testTotalArrecadado(){
+        assertEquals(1, veiculo.totalArrecadado());
+    }
+
+    @Test
+    public void testArrecadadoNoMes(){
+        int mes = 10;
+        assertEquals(1, veiculo.arrecadadoNoMes(mes));
+    }
+
+    @Test
+    public void testTotalDeUsos(){
+        assertEquals(1, veiculo.totalDeUsos());
+    }
+}
