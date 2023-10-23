8c34fbc01ebf975cc761180c6d94a4cae03fa57e
Gustavo Barbosa <gustavomb1313@gmail.com>
2023-10-23 09:17:11 -0300

    implementação metodo calculartempoestacionado

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -7,18 +7,21 @@ public class Vaga {
     private boolean disponivel;
     private Cliente cliente; // Representa o cliente que está ocupando a vaga no momento
     private List<RegistroHistorico> historico; // Para manter o histórico de uso da vaga
+    private long entradaMillis; // Momento em que o cliente estacionou na vaga
 
     public Vaga(int fila, int numero) {
         this.id = "Vaga " + fila + numero;
         this.disponivel = true;
         this.cliente = null;
         this.historico = new ArrayList<>();
+        this.entradaMillis = 0;
     }
 
     public boolean estacionar(Cliente cliente) {
         if (disponivel) {
             this.cliente = cliente;
             this.disponivel = false;
+            this.entradaMillis = System.currentTimeMillis();
             return true;
         } else {
             return false; // A vaga já está ocupada
@@ -54,12 +57,18 @@ public class Vaga {
     
 
     private double calcularTempoEstacionado() {
-        // Implementar ainda a lógica para calcular o tempo estacionado aqui !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        return 0.0;
+        if (cliente == null){
+            return 0.0;}
+             long tempoSaidaMillis = System.currentTimeMillis();
+
+            double tempoEstacionado = (tempoSaidaMillis - entradaMillis) / (1000.0 * 60.0); // Calcula o tempo estacionado em minutos
+
+            return tempoEstacionado;
     }
 
     double calcularValorCobranca(double tempoEstacionado) {
-        // Valor por fração de tempo (15 minutos), Limite máximo de cobrança, Calcular o número de frações de tempo (15 minutos) arredondando para cima, 
+        // Valor por fração de tempo (15 minutos), Limite máximo de cobrança, 
+        // Calcular o número de frações de tempo (15 minutos) arredondando para cima, 
 		// Calcular o valor da cobrança, Verificar se o valor ultrapassou o limite
     double valorPorFracao = 4.0; 
     double limiteCobranca = 50.0;
--- codigo/VagaTest.java
+++ codigo/VagaTest.java
@@ -1,32 +1,32 @@
-   import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class VagaTest {
-        private Vaga vaga;
+    private Vaga vaga;
 
-
-        @BeforeEach
-        public void setUp() {
+    @BeforeEach
+    public void setUp() {
         vaga = new Vaga(1, 1);
         Cliente cliente = new Cliente("Nome do Cliente", "123456");
         vaga.estacionar(cliente);
     }
 
-        @Test
-        public void testCalcularValorCobranca() {
-            // Valor esperado com base na lógica de cálculo, 40.0 para representar 40 minutos
-            assertEquals(12, vaga.calcularValorCobranca(40), 0.01); // (0.01) para números de ponto flutuante
-        }
+    @Test
+    public void testCalcularValorCobranca() {
+        // Valor esperado com base na lógica de cálculo, 40.0 para representar 40
+        // minutos
+        assertEquals(12, vaga.calcularValorCobranca(40), 0.01); // (0.01) para números de ponto flutuante
+    }
 
-        @Test
-        public void testEstacionar(){
-            assertEquals(false,vaga.estacionar(null)); 
-        }
+    @Test
+    public void testEstacionar() {
+        assertEquals(false, vaga.estacionar(null));
+    }
 
-        @Test
-        public void testDisponivel(){
-            assertEquals(false,vaga.isDisponivel());
-        }
+    @Test
+    public void testDisponivel() {
+        assertEquals(false, vaga.isDisponivel());
+    }
 }
