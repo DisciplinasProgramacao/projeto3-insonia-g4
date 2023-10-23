eeecc4fed2b945df4eed943b625441b54f3b6d4f
Gustavo Barbosa <gustavomb1313@gmail.com>
2023-10-23 09:18:17 -0300

    metodo calcular tempo estacionado

--- codigo/Estacionamento_Classes/Vaga.java
+++ codigo/Estacionamento_Classes/Vaga.java
@@ -7,18 +7,21 @@ public class Vaga {
     private boolean disponivel;
     private Cliente cliente; // Representa o cliente que está ocupando a vaga no momento
     private List<RegistroHistorico> historico; // Para manter o histórico de uso da vaga
+    private long entradaMillis; // Momento em que o cliente estacionou na vaga
 
     public Vaga(int fila, int numero) {
         this.id = "Vaga " + fila + numero;
         this.disponivel = true;
         this.cliente = null;
         this.historico = new ArrayList<>();
+        this.entradaMillis = 0;
     }
 
     public boolean estacionar(Cliente cliente) {
         if (disponivel) {
             this.cliente = cliente;
             this.disponivel = false;
+            this.entradaMillis = System.currentTimeMillis();
             return true;
         } else {
             return false; // A vaga já está ocupada
@@ -54,8 +57,13 @@ public class Vaga {
     
 
     private double calcularTempoEstacionado() {
-        // Implementar ainda a lógica para calcular o tempo estacionado aqui !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        return 0.0;
+        if (cliente == null){
+            return 0.0;}
+             long tempoSaidaMillis = System.currentTimeMillis();
+
+            double tempoEstacionado = (tempoSaidaMillis - entradaMillis) / (1000.0 * 60.0); // Calcula o tempo estacionado em minutos
+
+            return tempoEstacionado;
     }
 
     double calcularValorCobranca(double tempoEstacionado) {
