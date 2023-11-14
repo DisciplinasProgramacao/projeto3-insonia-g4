2cda8952e9627fc122b54e5ec2b26004fde0bf93
Luiz Victor Aldenucci <105724710+DevtecAbrasel@users.noreply.github.com>
2023-11-14 15:21:19 -0300

    classificaçao

--- codigo/Estacionamento_Classes/Cliente.java
+++ codigo/Estacionamento_Classes/Cliente.java
@@ -1,75 +1,99 @@
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-/**
- * <h1>Cliente</h1>
- * Classe Cliente.
- * @author Gabriel Alejandro Figueiro Galindo
- * @since 10/10/2023
-*/
-public class Cliente implements Serializable{
-    private String nome;//Nome do cliente;
-    private String id;//Identificador;
-    private Map<String, Veiculo> veiculos;//Map para armazenar os veiculos, com placas de veículos como chaves;
 
-    //Constructor;
-    public Cliente(String nome, String id){
+enum Modalidade {
+    HORISTA, DE_TURNO, MENSALISTA
+}
+
+public class Cliente implements Serializable {
+    private String nome;
+    private String id;
+    private Map<String, Veiculo> veiculos;
+    private Modalidade modalidade;
+
+    // Construtor com a adição da modalidade
+    public Cliente(String nome, String id, Modalidade modalidade) {
         this.nome = nome;
         this.id = id;
+        this.modalidade = modalidade;
         this.veiculos = new HashMap<>();
     }
 
-	//Getters;
-	public String getNome(){return nome;}
-	public String getID(){return id;}
-	public Map<String, Veiculo> getVeiculos(){return veiculos;}
+    // Métodos getters
+    public String getNome() {
+        return nome;
+    }
 
-    //Método para adicionar Veiculo para o Map de veículos;
-    public void addVeiculo(Veiculo veiculo){
-        if(veiculos.containsKey(veiculo.getPlaca())){
+    public String getID() {
+        return id;
+    }
+
+    public Map<String, Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    // Método para adicionar veículo com base na modalidade
+    public void addVeiculo(Veiculo veiculo) {
+        if (veiculos.containsKey(veiculo.getPlaca())) {
             throw new IllegalArgumentException("Erro - Carro já inserido.");
         }
         veiculos.put(veiculo.getPlaca(), veiculo);
     }
 
-	//Verifique se o Cliente possui um Veículo com uma placa específica;
-    public Veiculo possuiVeiculo(String placa){
+    // Método para calcular o valor a ser cobrado pelo cliente
+    public double calcularCobranca() {
+        switch (modalidade) {
+            case HORISTA:
+                return calcularCobrancaHorista();
+            case DE_TURNO:
+                return 200; // Mensalidade fixa de R$200
+            case MENSALISTA:
+                return 500; // Mensalidade fixa de R$500
+            default:
+                throw new IllegalArgumentException("Modalidade inválida");
+        }
+    }
+
+    // Método para calcular a cobrança para clientes horistas
+    private double calcularCobrancaHorista() {
+        double totalArrecadado = 0;
+        for (Veiculo veiculo : veiculos.values()) {
+            totalArrecadado += veiculo.totalArrecadado();
+        }
+        return totalArrecadado;
+    }
+
+    // Outros métodos permanecem inalterados
+
+    //Método para verificar se o cliente possui um veículo com uma placa específica
+    public Veiculo possuiVeiculo(String placa) {
         return veiculos.get(placa);
     }
 
-	//Total de usos;
-	public int totalDeUsos(){
-		int totalDeUsos = 0;
-		for(Veiculo veiculo : veiculos.values()){
-			totalDeUsos += veiculo.totalDeUsos();
+    //Método para obter o total de usos
+    public int totalDeUsos() {
+        int totalDeUsos = 0;
+        for (Veiculo veiculo : veiculos.values()) {
+            totalDeUsos += veiculo.totalDeUsos();
         }
         return totalDeUsos;
     }
 
-	//Arrecadado pelo veiculo com a placa específica;
-	public double arrecadadoPorVeiculo(String placa){
-		Veiculo veiculo = veiculos.get(placa);
-		if(veiculo == null){
-			throw new IllegalArgumentException("Erro - Veiculo não encontrado.");
-		}
-		return veiculo.totalArrecadado();
-	}
+    //Método para obter o valor arrecadado por um veículo com uma placa específica
+    public double arrecadadoPorVeiculo(String placa) {
+        Veiculo veiculo = veiculos.get(placa);
+        if (veiculo == null) {
+            throw new IllegalArgumentException("Erro - Veículo não encontrado.");
+        }
+        return veiculo.totalArrecadado();
+    }
 
-	//Total arrecadado pelo Cliente;
-    public double arrecadadoTotal(){
-        double TotalArrecadado = 0;
-        for(Veiculo veiculo : veiculos.values()){
-            TotalArrecadado += veiculo.totalArrecadado();
+    //Método para obter o total arrecadado pelo cliente
+    public double arrecadadoTotal() {
+        double totalArrecadado = 0;
+        for (Veiculo veiculo : veiculos.values()) {
+            totalArrecadado += veiculo.totalArrecadado();
         }
-        return TotalArrecadado;
+        return totalArrecad
     }
-	
-    //Arrecadado no mês pelo Cliente;
-    public double arrecadadoNoMes(int mes){
-        double ArrecadadoMes = 0;
-        for(Veiculo veiculo : veiculos.values()){
-            ArrecadadoMes += veiculo.arrecadadoNoMes(mes);
-        }
-        return ArrecadadoMes;
-    }
-}
