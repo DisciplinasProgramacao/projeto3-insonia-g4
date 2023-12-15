602f04b3a26565335e6f474e1f12007bb44530dd
Gabrielle Lira <79920434+GabiLira@users.noreply.github.com>
2023-12-15 17:24:55 -0300

    Update and rename Relatorio.java to RelatorioCompleto.java

--- codigo/Estacionamento_Classes/RelatorioCompleto.java
+++ codigo/Estacionamento_Classes/RelatorioCompleto.java
@@ -1,23 +1,33 @@
+import java.text.SimpleDateFormat;
 import java.util.*;
-import java.io.Serializable;
 
-public class Relatorio implements Observer, Serializable{
+public class RelatorioCompleto implements Observer, Serializable {
     private Map<Cliente, Double> top5Clientes;
     private double arrecadacaoMensal;
 
-    public Relatorio() {
-        this.top5Clientes = new LinkedHashMap<>();// LinkedHashMap para manter a ordem de inserção;
+    public RelatorioCompleto() {
+        this.top5Clientes = new LinkedHashMap<>();
         this.arrecadacaoMensal = 0.0;
+    }
+
+    public void gerarRelatorio(Veiculo veiculo) {
+        veiculo.ordenarUsosPorData();
+        System.out.println("Relatório de Usos de Vaga para o Veículo de Placa: " + veiculo.getPlaca());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        for (int i = 0; i < veiculo.getTotalUsos(); i++) {
+            UsoDeVaga uso = veiculo.getUsos()[i];
+            Date data = uso.getData();
+            String dataFormatada = dateFormat.format(data);
+            System.out.println("Data: " + dataFormatada + " - Vaga: " + uso.getVaga().toString());
+        }
     }
 
     @Override
     public void updateArrecadacao(Cliente cliente, double novaArrecadacao) {
         top5Clientes.put(cliente, novaArrecadacao);
-
-        // Ordena o Top5 pelos valores de arrecadação em ordem decrescente;
         top5Clientes = sortByValue(top5Clientes);
 
-        // Mantém apenas os 5 primeiros clientes no Top5;
         if (top5Clientes.size() > 5) {
             Iterator<Map.Entry<Cliente, Double>> iterator = top5Clientes.entrySet().iterator();
             Map.Entry<Cliente, Double> entry = null;
@@ -28,8 +38,6 @@ public class Relatorio implements Observer, Serializable{
                 iterator.remove();
             }
         }
-
-        // Atualiza a arrecadação mensal;
         arrecadacaoMensal += novaArrecadacao;
     }
 
@@ -41,7 +49,6 @@ public class Relatorio implements Observer, Serializable{
         return arrecadacaoMensal;
     }
 
-    // Método utilitário para ordenar um Map por valores;
     private <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
         List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
         list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
