dd94cdce5e7f0d6f77bd969fdd4bc3c5bb92f44e
gabriel6988 <83712515+gabriel6988@users.noreply.github.com>
2023-12-15 22:33:05 -0300

    Concertando novos erros;

--- codigo/Estacionamento_Classes/Testes/EstacionamentoTest.java
+++ codigo/Estacionamento_Classes/Testes/EstacionamentoTest.java
@@ -2,8 +2,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class EstacionamentoTest {
 
+    static List<Estacionamento> estacionamentos = new ArrayList<>();
     private Estacionamento estacionamento;
     private Cliente cliente;
 
@@ -12,12 +16,13 @@ class EstacionamentoTest {
         estacionamento = new Estacionamento("Nome", 5, 10);
         cliente = new Cliente("Cliente1", "ID1", Modalidade.HORISTA);
         estacionamento.addCliente(cliente);
+        estacionamentos.add(estacionamento);
     }
 
     @Test
     void testArrecadacaoMediaClientesHoristas() {
-        // Considerando que o método arrecadacaoMediaClientesHoristas está correto
-        double arrecadacaoMedia = estacionamento.arrecadacaoMediaClientesHoristas(1);
+        // Considerando que o método arrecadacaoMediaClientesHoristas está correto;
+        double arrecadacaoMedia = Estacionamento.arrecadacaoMediaClientesHoristas(1, estacionamentos);
         assertEquals(0.0, arrecadacaoMedia);
     }
 
@@ -31,29 +36,29 @@ class EstacionamentoTest {
 
     @Test
     void testTotalArrecadado() {
-        // Considerando que o método totalArrecadado está correto
+        // Considerando que o método totalArrecadado está correto;
         double totalArrecadado = estacionamento.totalArrecadado();
         assertEquals(0.0, totalArrecadado);
     }
 
     @Test
     void testArrecadacaoNoMes() {
-        // Considerando que o método arrecadacaoNoMes está correto
+        // Considerando que o método arrecadacaoNoMes está correto;
         double arrecadacaoNoMes = estacionamento.arrecadacaoNoMes(1);
         assertEquals(0.0, arrecadacaoNoMes);
     }
 
     @Test
     void testMediaClientesMensal() {
-        // Considerando que o método mediaClientesMensal está correto
+        // Considerando que o método mediaClientesMensal está correto;
         double mediaClientesMensal = estacionamento.mediaClientesMensal();
         assertEquals(0.0, mediaClientesMensal);
     }
 
     @Test
-    void testTop5Clientes() {
-        // Considerando que o método top5Clientes está correto
-        String top5Clientes = estacionamento.top5Clientes(1);
+    public static void testTop5Clientes() {
+        // Considerando que o método top5Clientes está correto;
+        String top5Clientes = Estacionamento.top5Clientes(1, estacionamentos);
         assertEquals("", top5Clientes);
     }
 }
